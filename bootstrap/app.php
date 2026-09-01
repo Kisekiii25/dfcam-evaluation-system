cb41ccfd01b8918bce0ceb5d8a11fcd97f6f3c85
kisekiii25 <lunozakeivinchristian@gmail.com>
2026-09-01 10:21:42 +0800

    fix: trust proxies and force https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies for Render load balancing & HTTPS termination
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
